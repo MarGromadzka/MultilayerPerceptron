02da186f64b1f03279904eb858fb60d82c46cd60
Marianna Gromadzka <mgromadz00@gmail.com>
2021-01-04 19:10:07 +0100

    preparation

--- MultilayerPerceptron/data_preparation.py
+++ MultilayerPerceptron/data_preparation.py
@@ -1,1 +1,32 @@
+import numpy as np
+from mlxtend.data import loadlocal_mnist
 
+
+def get_train_data():
+    """ zwraca treningowe znormalizowane dane, X to obrazy, y to odpowiadająca mu cyfry"""
+    if not platform.system() == 'Windows':
+        X, y = loadlocal_mnist(
+                images_path='train-images-idx3-ubyte', 
+                labels_path='train-labels-idx1-ubyte')
+    else:
+        X, y = loadlocal_mnist(
+                images_path='train-images.idx3-ubyte', 
+            labels_path='train-labels.idx1-ubyte')
+
+    X = X.astype(float) / 255
+    return X, y
+
+def get_test_data():
+    """ zwraca testowe znormalizowane dane, X to obrazy, y to odpowiadająca mu cyfry"""
+    if not platform.system() == 'Windows':
+        X, y = loadlocal_mnist(
+                images_path='t10k-images-idx3-ubyte', 
+                labels_path='t10k-labels-idx1-ubyte')
+    else:
+        X, y = loadlocal_mnist(
+                images_path='t10k-images.idx3-ubyte', 
+            labels_path='t10k-labels.idx1-ubyte')
+
+    X = X.astype(float) / 255
+
+    return X,y
